218ec97c05e50494fab4a2ebec0f6ec8eec9877b
Simone <startae14@gmail.com>
2021-02-14 17:48:02 +0100

    passing props from parent to child components

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import faker from 'faker';
 
-const CommentDetail = () => (
+const CommentDetail = ({ author }) => (
   <div className="comment">
     <a href="/" className="avatar">
       <img alt="avatar" src={faker.image.image()} />
     </a>
     <div className="content">
       <a href="/" className="author">
-        Sam
+        {author}
       </a>
       <div className="metadata">
         <span className="date">Today at 6:00PM</span>
@@ -17,3 +17,5 @@ const CommentDetail = () => (
     </div>
   </div>
 );
+
+export default CommentDetail;
