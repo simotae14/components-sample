cb080882adeb42b895a32445f45b073940c276bc
Simone <startae14@gmail.com>
2021-02-14 18:30:42 +0100

    use the children Props

--- src/index.js
+++ src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from './ApprovalCard';
 
 const App = () => (
   <div className="ui container comments">
-    <ApprovalCard />
-    <CommentDetail
-      author="Sam"
-      timeAgo="Today at 4.45PM"
-      content="Nice blog post!"
-      avatar={faker.image.image()}
-    />
-    <CommentDetail
-      author="Alex"
-      timeAgo="Today at 2.00AM"
-      content="I like the subject"
-      avatar={faker.image.image()}
-    />
-    <CommentDetail
-      author="Jane"
-      timeAgo="Yesterday at 5:00PM"
-      content="I like the writing"
-      avatar={faker.image.image()}
-    />
+    <ApprovalCard>
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4.45PM"
+        content="Nice blog post!"
+        avatar={faker.image.image()}
+      />
+    </ApprovalCard>
+    <ApprovalCard>
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2.00AM"
+        content="I like the subject"
+        avatar={faker.image.image()}
+      />
+    </ApprovalCard>
+    <ApprovalCard>
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        content="I like the writing"
+        avatar={faker.image.image()}
+      />
+    </ApprovalCard>
   </div>
 );
 
