4a89ac69095d2601b4ac012ae47058c934dea810
Simone <startae14@gmail.com>
2021-02-14 17:00:27 +0100

    add a Comment component from the semantic ui library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <div>Hi there!</div>;
+const App = () => (
+  <div className="ui container comments">
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Sam
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 6:00PM</span>
+        </div>
+        <div className="text">Nice blog post!</div>
+      </div>
+    </div>
+  </div>
+);
 
 ReactDOM.render(<App />, document.querySelector('#root'));
